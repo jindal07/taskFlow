7273ede9fd9f9bfc6956649b4e30dd0a08bfb503
Harsh Jindal <121694647+jindal07@users.noreply.github.com>
2025-06-17 20:42:22 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'TaskFlow',
+  description: 'To do list app',
+  generator: 'Harsh Jindal',
 }
 
 export default function RootLayout({
